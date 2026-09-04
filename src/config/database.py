20ae37345b279c1487de00fd06ccacb891de3603
Sidnei Júnior <fariasidnei48@gmail.com>
2026-09-04 19:10:57 -0300

    chore: adiciona suporte ao banco em nuvem

--- src/config/database.py
+++ src/config/database.py
@@ -1,20 +1,22 @@
 import os
 
 import psycopg
-
 from dotenv import load_dotenv
 
 
-# Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
 
 
-# Cria e retorna uma conexão com o PostgreSQL
 def get_connection():
+    database_url = os.getenv("DATABASE_URL")
+
+    if database_url:
+        return psycopg.connect(database_url)
+
     return psycopg.connect(
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
+        password=os.getenv("DB_PASSWORD")
     )
